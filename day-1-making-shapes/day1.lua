7957eef66e923fc3fd45fdc91332385c2d326b1a
Aleksandra Banasiak <aleksandra.banasiak@protonmail.com>
2022-12-13 00:00:11 +0100

    Day 1 submission

---
+++ day-1-making-shapes/day1.lua
@@ -0,0 +1,22 @@
+-- author:  Spook
+-- desc:    Tiny Code Christmas day 1
+-- script:  lua
+
+function TIC()
+	o=tri
+	e=100
+	f=150
+	g=120
+	h=60
+	o(e,h,h,e,f,85,14)
+	o(h,e,f,85,75,105,13)
+	o(e,h,e,20,g,45,14)
+	o(e,20,g,45,g,2,13)
+	o(e,h,130,30,180,130,2)
+	o(g,80,130,50,160,30,14)
+	o(130,50,160,30,f,5,13)
+	o(e,h,110,50,g,80,12)
+	circ(180,130,5,12)
+	circ(e,70,3,5)
+	circ(e,73,3,14)
+end
